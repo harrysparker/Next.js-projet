391bcd7837144ff842ded2edeff135980708e119
Guy Mosso <guymosso@192.168.1.8>
2024-09-18 17:16:30 -0700

    Fin

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana} lusitana`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Bienvenu sur Parker</strong> Votre première App{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              
             </a>
-            , brought to you by Vercel.
+            
           </p>
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
           <Link
