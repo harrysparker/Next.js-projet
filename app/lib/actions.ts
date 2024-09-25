5753898b9a50b518c1c3948092988bcd97d659ad
Harrys Parker <harrysparker@192.168.1.10>
2024-09-25 08:52:04 +0000

    relusse

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,13 +11,13 @@ import { signIn } from "@/auth";
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({ invalid_type_error: "Please select a customer." }),
-  amount: z.coerce.number()
+  amount: z.coerce
+  .number()
   .gt(0, { message: 'Please enter an amount greater than $0.' }),
 
   status: z.enum(["pending", "paid"], {
     invalid_type_error: 'Please select an invoice status.',
   }),
-
   date: z.string(),
 });
 
