848efec43780e622de52b53b9dc716d005e42ba9
Harrys Parker <harrysparker@192.168.1.10>
2024-09-25 12:19:55 +0000

    Retouche

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,30 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from 'next';
+import CustomersTable from "@/app/ui/customers/table";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+
+export default async function Page(
+  {
+    searchParams
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+
+    }
+  }
+
+) {
+  const query = searchParams?.query || ""
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
